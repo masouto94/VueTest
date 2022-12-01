2fa1ab222d3343e3a13946c8ef1112159f72e2d5
Matias Souto <masouto94@gmail.com>
2022-12-01 16:19:03 -0300

    First steps with VUECli

---
+++ VueTest/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    
+    <Header title="Task Tracker"/>
+    <Tasks @delete-task="deleteTask" :tasks="tasks"/>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue"
+import Tasks from "./components/Tasks.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  methods:{
+    deleteTask(id){
+      if(confirm("Borrar evento?")){
+      this.tasks = this.tasks.filter((task)=> task.id !== id)
+    }
+  }
+  },
+  created() {
+    this.tasks = [{
+      id:1,
+      text: 'Primer tarea',
+      day: 'Domingo',
+      reminder: true
+    },
+    {
+      id:2,
+      text: 'Segunda tarea',
+      day: 'Lunes',
+      reminder: true
+    },
+    {
+      id:3,
+      text: 'Tercera tarea',
+      day: 'Miércoles',
+      reminder: false
+    },
+  ]
+  }
+}
+</script>
+<style scoped>
+.container{
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border:1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px
+}
+</style>
---
+++ VueTest/src/components/Button.vue
@@ -0,0 +1,40 @@
+<template>
+    <button :style="{background: color}" @click="onClick()">
+        {{text}}
+    </button>
+</template>
+
+<script>
+export default{
+    name: "Button",
+    props:{
+        text: {
+            type:String
+        },
+        color:{
+            type: String,
+            default: "black"
+        }
+    },
+    methods:{
+        onClick(){
+            console.log("Hola")
+        }
+    }
+}
+</script>
+
+<style scoped>
+button{
+    display: inline-block;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    font-size: 15px;
+    font-family: inherit;
+}
+</style>
